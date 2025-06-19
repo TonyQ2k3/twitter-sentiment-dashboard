ccddfcc4489602607ea62eb21c2b7c3085f9325d
TonyQ2k3 <ntbquoc2015@gmail.com>
2025-06-19 21:41:17 +0700

    Minor fixes

--- frontend/src/pages/Monitor.jsx
+++ frontend/src/pages/Monitor.jsx
@@ -45,7 +45,7 @@ const Sidebar = ({ selectedPanel, onPanelSelect }) => {
     },
     { 
       id: 'dataset-drift', 
-      name: 'Dataset Drift',
+      name: 'Data Drift',
       icon: (
         <svg className="w-5 h-5 mr-3" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
           <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M7 12l3-3 3 3 4-4M8 21l4-4 4 4M3 4h18M4 4h16v12a1 1 0 01-1 1H5a1 1 0 01-1-1V4z"></path>
